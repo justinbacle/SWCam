441f0a9148deab7182087ec3b8fd01b5b1e3a608
Justin Bacle <justin.bacle@live.fr>
2021-04-01 00:54:18 +0200

    added default linux cti gentl path

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 if sys.platform == "linux":
-    CTI_FILEPATH = ""
+    CTI_FILEPATH = "/opt/spinnaker/lib/flir-gentl/FLIR_GenTL.cti"
 else:
     CTI_FILEPATH = "C:/Program Files/FLIR Systems/Spinnaker/bin64/vs2015/FLIR_GenTL_v140.cti"
 
