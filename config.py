97bc96586f2cadd2c0ff6bcdfb52905acf68cacd
Justin Bacle <justin.bacle@live.fr>
2021-04-01 00:26:49 +0200

    added switch for CTI Filepath

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
 import os
+import sys
 
-CTI_FILEPATH = "C:/Program Files/FLIR Systems/Spinnaker/bin64/vs2015/FLIR_GenTL_v140.cti"
+if sys.platform == "linux":
+    CTI_FILEPATH = ""
+else:
+    CTI_FILEPATH = "C:/Program Files/FLIR Systems/Spinnaker/bin64/vs2015/FLIR_GenTL_v140.cti"
+
 MAKEDNG_PATH = os.getcwd() + "/makeDNG/makeDNG.exe"
