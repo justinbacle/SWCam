5570f55c030698447c8903d6b2432b9251012cf6
Justin Bacle <justin.bacle@live.fr>
2021-06-24 18:14:36 +0200

    linux config for pyside

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 import os
 import sys
+import PySide2
 
 if sys.platform == "linux":
     CTI_FILEPATH = "/opt/spinnaker/lib/flir-gentl/FLIR_GenTL.cti"
+
+    # PySide default configuration is messed up in linux
+    dirname = os.path.dirname(PySide2.__file__)
+    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
 else:
     CTI_FILEPATH = "C:/Program Files/FLIR Systems/Spinnaker/bin64/vs2015/FLIR_GenTL_v140.cti"
-
-MAKEDNG_PATH = os.getcwd() + "/makeDNG/makeDNG.exe"
+    MAKEDNG_PATH = os.getcwd() + "/makeDNG/makeDNG.exe"
